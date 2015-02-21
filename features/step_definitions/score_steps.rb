8a66a82ed765345999d560bcb113eac4d283154e
mromphf <markromphf@gmail.com>
2015-02-21 14:23:15 -0600

    Flesh out score feature steps and add lib file for player class.

--- features/step_definitions/score_steps.rb
+++ features/step_definitions/score_steps.rb
@@ -1,16 +1,20 @@
-When(/^the dealer deals me a (\d+) of hearts$/) do |arg1|
-    pending # express the regexp above with the code you wish you had
+require_relative "../../lib/player.rb"
+require_relative "../../lib/card.rb"
+
+When(/^the dealer deals me a (\d+) of hearts$/) do |value|
+  @player = Player.new
+  @player.add_card(Card.new(value, :hearts))
 end
 
-When(/^the dealer deals me a jack of clubs$/) do
-    pending # express the regexp above with the code you wish you had
+When(/^the dealer deals me a (\d+) of clubs$/) do |value|
+  @player.add_card(Card.new(value, :clubs))
 end
 
-When(/^the dealer deals me a (\d+) of spades$/) do |arg1|
-    pending # express the regexp above with the code you wish you had
+When(/^the dealer deals me a (\d+) of spades$/) do |value|
+  @player.add_card(Card.new(value, :spades))
 end
 
-Then(/^I should have a total score of (\d+)$/) do |arg1|
-    pending # express the regexp above with the code you wish you had
+Then(/^I should have a total score of (\d+)$/) do |value|
+  expect(@player.score).to eq value
 end
 
