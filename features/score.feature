ad3faf538fc61928b8007931d1592d3ad3c09d4c
mromphf <markromphf@gmail.com>
2015-02-21 14:15:56 -0600

    Add first cucumber feature for scoring.

---
+++ features/score.feature
@@ -0,0 +1,10 @@
+Feature: Scores
+    In order to beat the dealer and win chips
+    As a player
+    I want to be dealt cards to accumulate a score
+
+    Scenario: Add three cards to form a score
+        When the dealer deals me a 5 of hearts
+        And the dealer deals me a jack of clubs
+        And the dealer deals me a 2 of spades
+        Then I should have a total score of 17
---
+++ features/step_definitions/score_steps.rb
@@ -0,0 +1,16 @@
+When(/^the dealer deals me a (\d+) of hearts$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+When(/^the dealer deals me a jack of clubs$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+When(/^the dealer deals me a (\d+) of spades$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should have a total score of (\d+)$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
