66907a0ee22a64bd99f18d17c85bab2897a01808
Mark Romphf <markromphf@gmail.com>
2015-03-24 21:24:53 -0600

    Clean up some of the player specs.

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -20,26 +20,27 @@ describe Player do
       player = Player.new.add_card(Card.new(7, :diamonds))
       expect(player).to be_a Player
     end
+
+    it "will update score correctly when given new cards" do
+      player = Player.new.add_card(Card.new(6, :clubs))
+      expect(player.score).to eq 6
+    end
   end
 
   describe "score " do
     it "is computed by the total value of each card in the players hand" do
-      player = Player.new.add_card(Card.new(7, :diamonds))
-      player = player.add_card(Card.new(4, :clubs))
-      player = player.add_card(Card.new(8, :spades))
-      expect(player.score).to eq 19
+      cards = [Card.new(7, :diamonds), Card.new(4, :clubs), Card.new(8, :spades)]
+      expect(Player.new(cards).score).to eq 19
     end
 
     it "is computed correctly with aces" do
-      player = Player.new.add_card(Card.new(11, :spades))
-      player = player.add_card(Ace.new(:hearts))
-      expect(player.score).to eq 21
+      cards = [ Card.new(11, :spades), Ace.new(:hearts) ]
+      expect(Player.new(cards).score).to eq 21
     end
 
     it "is blackjack when the player has a face card and an ace" do
-      player = Player.new.add_card(Ace.new(:diamonds))
-      player = player.add_card(Card.new(13, :spades))
-      expect(player.blackjack?).to eq true
+      cards = [ Card.new(13, :spades), Ace.new(:diamonds) ]
+      expect(Player.new(cards).blackjack?).to eq true
     end
 
     it "always evaluates face cards to a value of ten" do
