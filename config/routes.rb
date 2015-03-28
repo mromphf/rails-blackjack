b7fddd743c98a4649205b8f377eaa48ae071db43
Mark Romphf <markromphf@gmail.com>
2015-03-27 23:50:07 -0600

    Switch games index to games show.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'games#index'
-  resources :games
+  root 'games#show'
+  match('/', to: 'games#show', via: 'get')
   match('/hit', to: 'games#hit', via: 'get')
   match('/stand', to: 'games#stand', via: 'get')
 end
