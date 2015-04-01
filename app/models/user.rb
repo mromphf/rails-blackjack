230aeb9efcf8cf0016e209fee883d73a9c11e296
Mark Romphf <markromphf@gmail.com>
2015-03-31 21:20:30 -0600

    Start speccing out how the user will win money.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   :has_secure_password
 
   attr_accessor :password, :password_confirmation
+
+  def win(cash)
+    self.money += cash
+  end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,7 +1,17 @@
 require 'spec_helper'
 
 describe User do
+  let(:user) { User.new(username: "jimbob", 
+                        password: "abc123", 
+                        password_confirmation: "abc123") }
+
   it "should be valid with a password" do
-    User.new(username: "jimbob", password: "abc123", password_confirmation: "abc123").should be_valid
+    user.should be_valid 
+  end
+
+  it "wins money" do
+    user.money = 500
+    user.win(100)
+    user.money.should == 600
   end
 end
