5e549cdde79e6ac7ac1c4a77013bdf4e755ab4d6
Mark Romphf <markromphf@gmail.com>
2015-06-06 15:42:46 -0600

    Add omniauth authentication method to the user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,17 @@ class User < ActiveRecord::Base
             on: :create)
   validates(:password, length: { minimum: 4 }, on: :create)
 
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
+
   def win!(bet)
     self.money += bet
     save!
