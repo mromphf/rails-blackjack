7409defdb1eed8a44688631995e2929bb8895829
Mark Romphf <markromphf@gmail.com>
2015-07-11 00:02:58 -0600

    Replace http 'gets' with 'posts' where appropriate.

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,14 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
 
-  match '/', to: 'sessions#new', via: 'get'
-  match '/guest_auth', to: "sessions#create_guest", via: 'get'
+  match '/', to: 'sessions#new', via: 'post'
+  match '/guest_auth', to: "sessions#create_guest", via: 'post'
   match '/signout', to: 'sessions#destroy', via: 'delete'
   match '/play', to: "games#show", via: 'get'
   match '/reset_funds', to: "games#reset_funds", via: 'get'
   match '/bet', to: 'games#bet', via: 'post'
-  match '/bust', to: 'games#bust', via: 'get'
-  match '/decide_results', to: 'games#decide_results', via: 'get'
-  match '/player_hit', to: 'games#player_hit', via: 'get'
-  match '/dealer_hit', to: 'games#dealer_hit', via: 'get'
-  match '/stand', to: 'games#stand', via: 'get'
+  match '/bust', to: 'games#bust', via: 'post'
+  match '/decide_results', to: 'games#decide_results', via: 'post'
+  match '/player_hit', to: 'games#player_hit', via: 'post'
+  match '/dealer_hit', to: 'games#dealer_hit', via: 'post'
 end
